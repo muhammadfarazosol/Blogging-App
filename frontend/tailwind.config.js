f78c3d537bba881a6c8bcc0e15e5221584c99855
muhammadfarazosol <muhammad.faraz@osoltech.com>
2024-10-08 15:56:17 +0500

    first commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensure this path is correct for your project
+  ],
+  theme: {
+    extend: {
+      screens: {},
+    },
+  },
+  plugins: [],
+};
