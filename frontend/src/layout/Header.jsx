628abdffde555087d61e8bbb9c8455391200bfca
muhammadfarazosol <muhammad.faraz@osoltech.com>
2024-10-18 21:08:22 +0500

    Updated Header Profile Icon

--- frontend/src/layout/Header.jsx
+++ frontend/src/layout/Header.jsx
@@ -4,9 +4,11 @@ import { FiMenu } from "react-icons/fi";
 import { IoCloseOutline } from "react-icons/io5";
 import ProfileImage from "../assests/ProfileImage.svg";
 import clsx from "clsx";
+import { IoChevronDownCircleOutline } from "react-icons/io5";
 
 const Header = () => {
   const [isSideMenuOpen, setMenu] = useState(false);
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
 
   const navlinks = [
     {
@@ -20,6 +22,25 @@ const Header = () => {
     {
       label: "Authors",
       link: "/authors",
+    },
+  ];
+
+  const DropdownNavlinks = [
+    {
+      label: "Faraz",
+      link: "/profile/sdfsd",
+    },
+    {
+      label: "Create Post",
+      link: "/create",
+    },
+    {
+      label: "Authors",
+      link: "/authors",
+    },
+    {
+      label: "Logout",
+      link: "/logout",
     },
   ];
 
@@ -77,14 +98,37 @@ const Header = () => {
           </section>
         </div>
 
-        <section>
-          <img
-            src={ProfileImage}
-            alt="Profile Image Icon"
-            width={40}
-            height={40}
-            className="h-8 w-8 rounded-full"
-          />
+        <section className="relative">
+          <button
+            onClick={() => setDropdownOpen(!isDropdownOpen)}
+            className="flex items-center space-x-2 focus:outline-none"
+          >
+            <img
+              src={ProfileImage}
+              alt="Profile Image Icon"
+              width={40}
+              height={40}
+              className="h-8 w-8 rounded-full"
+            />
+            <IoChevronDownCircleOutline
+              className={`h-4 w-4 text-gray-600 transition-transform ${
+                isDropdownOpen ? "rotate-180" : ""
+              }`}
+            />
+          </button>
+          {isDropdownOpen && (
+            <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-10">
+              {DropdownNavlinks.map((item, index) => (
+                <Link
+                  key={index}
+                  to={item.link}
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-black hover:text-white transition-colors"
+                >
+                  {item.label}
+                </Link>
+              ))}
+            </div>
+          )}
         </section>
       </nav>
       <hr />
