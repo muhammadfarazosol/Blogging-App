eaaa38700e2a8169da2347f33eba0a3e605e2792
muhammadfarazosol <muhammad.faraz@osoltech.com>
2024-10-19 08:39:22 +0500

    User ( Authors ) APIs created and tested

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,26 @@
+const { Router } = require("express");
+
+const {
+  registerUser,
+  loginUser,
+  getUser,
+  changeAvatar,
+  editUser,
+  getAuthors,
+} = require("../controllers/userControllers.js");
+const authMiddleware = require("../middleware/authMiddleware.js");
+
+const router = Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/:id", getUser);
+router.get("/", getAuthors);
+router.post("/change-avatar", authMiddleware, changeAvatar);
+router.patch("/edit-user", authMiddleware, editUser);
+
+router.get("/", (req, res, next) => {
+  res.json("this is user route");
+});
+
+module.exports = router;
